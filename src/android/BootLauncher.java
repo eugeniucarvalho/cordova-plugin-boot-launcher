117f63338eb4d4e1a6c7ad64f858c8a1ccc1d7ce
unknown <eugeniucarvalho@gmail.com>
2016-01-10 11:32:30 -0300

    Change Main Class

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -8,7 +8,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.ionicframework.entregador885515.MainActivity.class);
+        Intent serviceIntent = new Intent(context, com.ionicframework.appentregador860269.MainActivity.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
